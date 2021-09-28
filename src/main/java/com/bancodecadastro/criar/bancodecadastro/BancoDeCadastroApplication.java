fc79793e2de8e6f244124f1d423ac68f15168832
wagnergunz <galaxdia@gmail.com>
2021-09-28 09:40:09 -0300

    Fisrt commit, criei o Controller parqa testar no heroku.

---
+++ src/main/java/com/bancodecadastro/criar/bancodecadastro/BancoDeCadastroApplication.java
@@ -0,0 +1,13 @@
+package com.bancodecadastro.criar.bancodecadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoDeCadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoDeCadastroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bancodecadastro/criar/bancodecadastro/BancoDeCadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancodecadastro.criar.bancodecadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoDeCadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
